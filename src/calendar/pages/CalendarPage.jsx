d2eb08505590b6d22e5e0aaa5d30e5948866f608
Samuel <spjara.aravena@gmail.com>
2022-08-28 23:13:08 -0400

    Front-end Completo

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,69 @@
-import { Navbar } from "../"
+import { useState } from 'react';
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { FabAddNew, FabDelete, Navbar, CalendarEventBox, CalendarModal } from '../';
+import { getMessagesES, localizer } from '../../helpers';
+import { useCalendarStore, useUiStore } from '../../hooks';
 
 export const CalendarPage = () => {
+
+    const { openDateModal } = useUiStore();
+    const { events, setActiveEvent } = useCalendarStore();
+    const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
+
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        const style = {
+            backgroundColor: '#347CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            color: 'white'
+        }
+
+        return {
+            style
+        }
+    }
+
+    const onDoubleClick = (event) => {
+        openDateModal();
+    }
+
+    const onSelect = (event) => {
+        setActiveEvent(event);
+    }
+
+    const onViewChange = (event) => {
+        localStorage.setItem('lastView', event);
+        setLastView(event);
+    }
+
     return (
         <>
             <Navbar />
+
+            <Calendar
+                culture='es'
+                localizer={localizer}
+                events={events}
+                defaultView={lastView}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 'calc( 100vh - 80px)' }}
+                messages={getMessagesES()}
+                eventPropGetter={eventStyleGetter}
+                components={{
+                    event: CalendarEventBox
+                }}
+                onDoubleClickEvent={onDoubleClick}
+                onSelectEvent={onSelect}
+                onView={onViewChange}
+
+            />
+
+            <CalendarModal />
+
+            <FabAddNew />
+            <FabDelete />
         </>
     )
 }
